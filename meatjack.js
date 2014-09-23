a5282f079a11a57501ea66ef00853e46c83ecc29
Myles Borins <myles@famo.us>
2014-09-23 02:33:44 -0700

    CLOSURED\!

--- meatjack.js
+++ meatjack.js
@@ -1,18 +1,21 @@
-document.body.appendChild(document.createElement('script')).src='https://rawgit.com/tommoor/tinycon/master/tinycon.min.js';
-// select the target node
-var target = document.querySelector('#chat-list');
+(function () {
+  document.body.appendChild(document.createElement('script')).src='https://rawgit.com/tommoor/tinycon/master/tinycon.min.js';
+  // select the target node
+  var target = document.querySelector('#chat-list');
 
-var count = 0;
-// create an observer instance
-var observer = new MutationObserver(function(mutations) {
-  var message = mutations[0].addedNodes[0].getElementsByTagName('p')[0].innerHTML
-  console.log(message);
-  count++;
-  Tinycon.setBubble(count);
-});
+  var count = 0;
+  // create an observer instance
+  var observer = new MutationObserver(function(mutations) {
+    var message = mutations[0].addedNodes[0].getElementsByTagName('p')[0].innerHTML
+    console.log(message);
+    count++;
+    Tinycon.setBubble(count);
+  });
 
-// configuration of the observer:
-var config = { attributes: true, childList: true, characterData: true };
+  // configuration of the observer:
+  var config = { attributes: true, childList: true, characterData: true };
 
-// pass in the target node, as well as the observer options
-observer.observe(target, config);
+  // pass in the target node, as well as the observer options
+  observer.observe(target, config);
+  
+}());
